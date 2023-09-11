3e574eb6e7ccc4942f5740d0f47f1697996b86a0
Dinesh Rout <rddinesh779@gmail.com>
2023-09-12 00:51:50 +0530

    feat: add to cart(ongoing) 🔨

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -9,7 +9,37 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      state.items.push(action.payload);
+      console.log(action.payload);
+      const itemExists = state.items.find(({ item }) => {
+        console.log('checking item:', item);
+        return item?.card?.info?.id === action.payload.card.info.id;
+      });
+
+      console.log('exists:', itemExists);
+
+      if (itemExists) {
+        state.items = state.items.map(({ item, quantity }) => {
+          console.log('item: ', item);
+          console.log('payload: ', action.payload);
+          if (item?.card?.info?.id === action.payload.card.info.id) {
+            return {
+              ...item,
+              quantity: quantity + 1,
+            };
+          } else {
+            return { item, quantity };
+          }
+        });
+      } else {
+        state.items.push({
+          item: action.payload,
+          quantity: 1,
+        });
+      }
+
+      // console.log('state: ', state);
+
+      // state.items.push(action.payload);
     },
     removeFromCart: (state, action) => {
       state.items = state.items.filter((item) => item.id !== action.payload.id);
