d29c3f3420e7454e18173b9980d8dc8db075e005
Dinesh Rout <rddinesh779@gmail.com>
2023-09-20 19:54:14 +0530

    refactor

--- src/hooks/useRestaurants.js
+++ src/hooks/useRestaurants.js
@@ -7,6 +7,7 @@ const useRestaurants = (url) => {
   const { address } = useSelector(selectAddress);
   const [banners, setBanners] = useState([]);
   const [foods, setFoods] = useState([]);
+  const [topRestaurants, setTopRestaurants] = useState([]);
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -19,31 +20,31 @@ const useRestaurants = (url) => {
       setIsLoading(true);
       const { data } = await axios.post(url, address);
 
-      console.log(data.data);
-
-      setBanners(
-        data?.data?.cards.filter(
-          (items) => items?.card?.card?.id === 'topical_banner'
-        )[0] ||
+      if (data?.data) {
+        setBanners(
           data?.data?.cards.filter(
             (items) => items?.card?.card?.id === 'topical_banner'
           )[0]
-      );
+        );
 
-      setFoods(
-        data?.data?.cards.filter(
-          (items) => items?.card?.card?.id === 'whats_on_your_mind'
-        )[0] ||
+        setFoods(
           data?.data?.cards.filter(
             (items) => items?.card?.card?.id === 'whats_on_your_mind'
           )[0]
-      );
+        );
 
-      setRestaurants(
-        data?.data?.cards.filter(
-          (items) => items?.card?.card?.id === 'restaurant_grid_listing'
-        )[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-      );
+        setTopRestaurants(
+          data?.data?.cards.filter(
+            (items) => items?.card?.card?.id === 'top_brands_for_you'
+          )[0]
+        );
+
+        setRestaurants(
+          data?.data?.cards.filter(
+            (items) => items?.card?.card?.id === 'restaurant_grid_listing'
+          )[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        );
+      }
     } catch (err) {
       console.log(err.response);
       setError(err.response);
@@ -59,6 +60,7 @@ const useRestaurants = (url) => {
   return {
     banners,
     foods,
+    topRestaurants,
     restaurants,
     isLoading,
     error,
